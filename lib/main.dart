5f24ef7f34b5fb8e97fc8a88351aa304776c22a0
Mina Ayman <mtrmena176@gmail.com>
2025-12-11 20:31:29 +0200

    dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ios_stopwatch/stop_watch_body.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
+      home: StopWatch(),
+    );
+  }
+}
+
+class StopWatch extends StatelessWidget {
+  const StopWatch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StopWatchBody(),
     );
   }
 }
