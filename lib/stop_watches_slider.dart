a67ae6ae5234941ad739320b63b0cc4ef563b801
Mina Ayman <mtrmena176@gmail.com>
2025-12-12 00:07:26 +0200

    slide betwen digital & analog watches

---
+++ lib/stop_watches_slider.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:ios_stopwatch/stop_watch_analog.dart';
+import 'package:ios_stopwatch/stop_watch_text.dart';
+
+class StopWatchesSlider extends StatefulWidget {
+  const StopWatchesSlider({super.key, required this.elapsed});
+  final Duration elapsed;
+
+  @override
+  State<StopWatchesSlider> createState() => _StopWatchesSliderState();
+}
+
+class _StopWatchesSliderState extends State<StopWatchesSlider> {
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 500,
+          child: PageView(
+            onPageChanged: (value) {
+              setState(() {
+                currentPage = value;
+              });
+            },
+            children: [
+              StopWatchText(elapsed: widget.elapsed),
+              StopWatchAnalog(elapsed: widget.elapsed),
+            ],
+          ),
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              2,
+              (index) => Container(
+                margin: EdgeInsets.all(8),
+                width: 8,
+                height: 8,
+                decoration: BoxDecoration(
+                  color: currentPage == index ? Colors.white : Colors.grey,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            )),
+      ],
+    );
+  }
+}
